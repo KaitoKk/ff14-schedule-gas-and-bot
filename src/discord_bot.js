74fbe7196c860b7a0a84442eccbcb55f38cacc13
Kaito Hasegawa <kaitohasegawa@KaitonoMac-mini.local>
2022-03-04 04:08:28 +0900

    メッセージにembededでリンクなどを追加できる

--- src/discord_bot.js
+++ src/discord_bot.js
@@ -8,21 +8,41 @@ class DiscordBot {
     this.ID = ""
   }
 
-  sendMessage(message) {
+  sendMessage(message, sheet_url = null, sheet_title = "スプレッドシート") {
     const url = `https://discord.com/api/webhooks/${this.ID}/${this.TOKEN}`
+
+    const payload = {
+      "token": this.TOKEN,
+      "channel": this.channel,
+      "content": message,
+      "username": this.username,
+      "parse": "full"
+    }
+
+    if(sheet_url !== null) {
+      payload["embeds"] = [
+        this._buildUrlEmbedObject(
+          sheet_title,
+          sheet_url
+        )
+      ]
+    }
     const params = {
       "method": "POST",
-      "payload": {
-        "token": this.TOKEN,
-        "channel": this.channel,
-        "content": message,
-        "username": this.username,
-        "parse": "full"
-      },
+      "contentType": "application/json",
+      "payload": JSON.stringify(payload),
       "muteHttpExceptions": true
     }
     
     const result = UrlFetchApp.fetch(url, params)
     Logger.log(result)
+  }
+
+  _buildUrlEmbedObject(title, url) {
+    return {
+      title,
+      url,
+      color: "6412288"
+    }
   }
 }
